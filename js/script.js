a8d57f44bf4b9f5ff2ea3c8b62396febf62f8d99
Samuel-sys <Alencar5647@gmail.com>
2023-03-13 16:36:46 -0300

    Primeira versão completa, ela reconhece a tecla que o player clica no teclado para que ele jogue.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const tema = document.querySelector('#tema');
 const palavra = document.querySelector('#palavra');
 
 //Objeto com o a palavra e o tema
-const forca = new Forca('fruta', 'Laranja');
+const forca = new Forca('fruta', 'uva');
 
 window.onload = function () {
     rederWord()
@@ -12,16 +12,27 @@ window.onload = function () {
 
 function rederWord() {
 
-    tema.innerHTML = forca.thema;
+    //Colocamos tema da palavra chave
+    if (tema.innerHTML != forca.thema) { tema.innerHTML = forca.thema; }
 
-    let word = forca.characters; 
+    palavra.innerHTML = '';
 
+    //Puxamos uma lista com todos os caracteres da palavra chave
+    let word = forca.characters;
     word.forEach((character) => {
+        //criamos uma palavra chave para cada caracter da palavra
         let element = document.createElement('span');
         element.classList.add('letra');
         element.innerHTML = character;
         palavra.appendChild(element);
-        console.log('element: ', element)
     });
+}
 
+window.onkeyup = function (x) {
+    if (x.key.length != 1 || parseInt(x.key) || x.key == '0' || x.key == ' ') {
+        alert('não e uma letra')
+    } else {
+        forca.move(x.key);
+        rederWord();
+    }
 }
--- model/forca.js
+++ model/forca.js
@@ -2,26 +2,68 @@ export class Forca {
     static hp = 6;
     #thema
     #word
-    #endGame
+    #revealed
+    endGame
+    #moves
 
     constructor(thema, word) {
         this.#thema = thema.toUpperCase();
         this.#word = word.toUpperCase();
-        this.#endGame = false;
+        this.endGame = false;
+        this.#moves = [];
+        this.#revealed = [...this.#word.replace(/[a-z]/gi, "_")];
     }
 
-    renderGame() {
-    }
-
-    get word() { return this.#word }
     get thema() { return this.#thema }
-    get characters() { return [...this.#word] }
-    
+
+    //get da palavra em List (array)
+    get #characters() { return [...this.#word] }
+    get characters() { return  this.#revealed }
 
     checkEndGame() {
         if (this.hp >= 0) {
-            this.#endGame = true;
-            alert('O jogo acabou');
+            this.endGame = true;
+            alert('O jogo acabou você perdeu');
+        } else if (this.#revealed.join('') == this.#word) {
+            alert('O jogo acabou você venceu a palavra secreta era ' + this.#word);
+            this.endGame = true;
         }
+    }
+
+    move(character) {
+
+        //Colocamos a letra em maiusculo para evitar erros bobos de letra maiuscula ou minuscula
+        character = character.toUpperCase();
+
+        //Verificamos se esse move já foi feito antes ele para a função aqui
+        if (this.#moves.includes(character)) {
+            alert('Você já testou essa letra');
+            return false;
+        } else {
+            //adiciona a letra na lista de movimentos já feitos no game
+            this.#moves.push(character);
+        }
+
+        //Se a letra que o usuario entrou constar na palavra ele inicia uma varredura na palavra para alterar o resultado do jogo
+        if (this.#word.includes(character)) {
+
+            //Pega a primeira posição (index) onde a letra aparece para registar na lista de indices
+            let posicao = this.#word.indexOf(character);
+            let indices = [];
+
+            //Só para de procurar quando não tiver mais essa letra na palavra
+            while (posicao !== -1) {
+                indices.push(posicao); //adiciona a posição onde foi encontrada a letra
+                posicao = this.#word.indexOf(character, posicao + 1); // começa a procurar uma possição a frente da ultima letra encontrada 
+            }
+
+            //Passamos por todos os indices encontrados na palavra e mostramos na palavra revelada para o usuario
+            indices.forEach(i => this.#revealed[i] = character);
+
+        } else {
+            this.hp--;
+        }
+
+        this.checkEndGame();
     }
 }
