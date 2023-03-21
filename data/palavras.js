4634f4192d8406325bd22869d5ce97b00c70aea0
Samuel-sys <Alencar5647@gmail.com>
2023-03-21 00:26:08 -0300

    Lista de palavras para usar no jogo

---
+++ data/palavras.js
@@ -0,0 +1,226 @@
+export const palavras = [
+    {
+        "palavra": "amizade",
+        "tema": "Relacionamentos"
+    },
+    {
+        "palavra": "amor",
+        "tema": "Relacionamentos"
+    },
+    {
+        "palavra": "família",
+        "tema": "Relacionamentos"
+    },
+    {
+        "palavra": "trabalho",
+        "tema": "Profissões"
+    },
+    {
+        "palavra": "advogado",
+        "tema": "Profissões"
+    },
+    {
+        "palavra": "médico",
+        "tema": "Profissões"
+    },
+    {
+        "palavra": "enfermeiro",
+        "tema": "Profissões"
+    },
+    {
+        "palavra": "professor",
+        "tema": "Profissões"
+    },
+    {
+        "palavra": "programador",
+        "tema": "Profissões"
+    },
+    {
+        "palavra": "engenheiro",
+        "tema": "Profissões"
+    },
+    {
+        "palavra": "arte",
+        "tema": "Cultura"
+    },
+    {
+        "palavra": "música",
+        "tema": "Cultura"
+    },
+    {
+        "palavra": "cinema",
+        "tema": "Cultura"
+    },
+    {
+        "palavra": "teatro",
+        "tema": "Cultura"
+    },
+    {
+        "palavra": "literatura",
+        "tema": "Cultura"
+    },
+    {
+        "palavra": "ciência",
+        "tema": "Educação"
+    },
+    {
+        "palavra": "história",
+        "tema": "Educação"
+    },
+    {
+        "palavra": "geografia",
+        "tema": "Educação"
+    },
+    {
+        "palavra": "matemática",
+        "tema": "Educação"
+    },
+    {
+        "palavra": "biologia",
+        "tema": "Educação"
+    },
+    {
+        "palavra": "química",
+        "tema": "Educação"
+    },
+    {
+        "palavra": "futebol",
+        "tema": "Esportes"
+    },
+    {
+        "palavra": "basquete",
+        "tema": "Esportes"
+    },
+    {
+        "palavra": "vôlei",
+        "tema": "Esportes"
+    },
+    {
+        "palavra": "natação",
+        "tema": "Esportes"
+    },
+    {
+        "palavra": "abacate",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "abacaxi",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "acerola",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "amora",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "caju",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "carambola",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "framboesa",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "goiaba",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "jaca",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "laranja",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "limão",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "manga",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "melancia",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "melão",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "morango",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "pera",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "uva",
+        "tema": "fruta"
+    },
+    {
+        "palavra": "bob esponja",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "cavalo de fogo",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "coragem o cão covarde",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "digimon",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "dragon ball",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "he-man",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "jaspion",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "mickey mouse",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "naruto",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "pica-pau",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "pokemon",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "scooby-doo",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "thundercats",
+        "tema": "desenho"
+    },
+    {
+        "palavra": "tom e jerry",
+        "tema": "desenho"
+    }
+]
